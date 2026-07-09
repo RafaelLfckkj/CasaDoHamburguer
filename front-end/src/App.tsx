cc547ff5c37b2ecc5ef9efda668d589d2569179f
RafaelLfckkj <rafaxolive@gmail.com>
2026-07-09 20:32:09 -0300

    criando navegação entre as paginas

--- front-end/src/App.tsx
+++
@@ -1,9 +0,0 @@
-
-export function App() {
-  return(
-    <div className="p-6 bg-black flex gap-2">
-        
-       
-    </div>
-  )
-}
---
+++ front-end/src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]);
