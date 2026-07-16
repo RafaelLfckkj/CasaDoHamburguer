e1b618c514f6fc1c0c709be987b177dc68299f3f
RafaelLfckkj <rafaxolive@gmail.com>
2026-07-15 23:46:42 -0300

    criando o primeiro endpoiont

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/teste", (req, res) => {
+    res.json("Voce Acessou a rota inicial")
+})
+
 app.listen(3000, () => {
     console.log("Servidor Rodando na porta 3000")
 });
