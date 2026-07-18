89b6575e528778cf8c874eb4786348c65832437c
RafaelLfckkj <rafaxolive@gmail.com>
2026-07-17 21:58:57 -0300

    estabelecendo a conexaão com o banco de dados pt1

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { connection } from "./src/db.js";
 
 const app = express();
+connection();
 
 app.post("/", (req, res) => {
     res.json("oie isso é um teste");
@@ -8,5 +10,5 @@ app.post("/", (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log("Servidor Rodando na porta 3000")
+    console.log("Servidor Rodando na porta 3000 ")
 });
